10cbea73df508085e519a377791bf70379b42c51
SANGWAN <38679417+perfectwan8765@users.noreply.github.com>
2024-02-21 21:59:06 +0900

    Title: 문자에 따른 명령 2, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 240221/문자에 따른 명령 2/text-based-commands2.py
+++ 240221/문자에 따른 명령 2/text-based-commands2.py
@@ -7,15 +7,12 @@ i = 0
 x, y = 0, 0
 for d in d_arr:
     if d == 'L':
-        i -= 1
+        i = (i-1+4)%4
     elif d == 'R':
-        i += 1
+        i = (i + 1)%4
     elif d == 'F':
         x += dx[i]
         y += dy[i]
     
-    if i == -1 :
-        i == 3
-    elif i == 4 :
-        i == 0
+    
 print(x, y)
