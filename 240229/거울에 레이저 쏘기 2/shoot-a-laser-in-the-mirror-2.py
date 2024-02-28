e37dfd5525edb7c924c2392e7414e720cc0c14d2
SANGWAN <38679417+perfectwan8765@users.noreply.github.com>
2024-02-29 00:06:55 +0900

    Title: 거울에 레이저 쏘기 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240229/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240229/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -30,10 +30,10 @@ while(True):
         if i == 0 :
             i = 3
         if i == 2 :
-            i = 3
+            i = 1
         if i == 3 :
             i = 0
-        if i == 3 :
+        if i == 1 :
             i = 2
     else :
         if i == 1 :
