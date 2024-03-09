d516cb5ab99a5901d3df2b04305848a9c2ce2e5e
SANGWAN <38679417+perfectwan8765@users.noreply.github.com>
2024-03-09 22:31:14 +0900

    Title: 체크판위에서 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240309/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,14 @@
+r, c = map(int, input().split())
+arr = [
+    input().split()
+    for _ in range(r)
+]
+
+result = 0
+for i in range(1, r) :
+    for j in range (1, c):
+        for k in range(i+1, r-1):
+            for l in range(j+1, c-1):
+                if arr[0][0] != arr[i][j] and arr[i][j] != arr[j][k] and arr[j][k] != arr[r-1][c-1]:
+                    result += 1
+print(result)
