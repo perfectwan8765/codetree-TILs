6df0749490071ba16f986cac3b8665de82946909
SANGWAN <38679417+perfectwan8765@users.noreply.github.com>
2024-03-09 22:32:11 +0900

    Title: 체크판위에서 2, Time: 127ms, Memory: 26MB, Status: Passed - CodeTree

--- 240309/체크판위에서 2/on-the-checkboard-2.py
+++ 240309/체크판위에서 2/on-the-checkboard-2.py
@@ -9,6 +9,6 @@ for i in range(1, r) :
     for j in range (1, c):
         for k in range(i+1, r-1):
             for l in range(j+1, c-1):
-                if arr[0][0] != arr[i][j] and arr[i][j] != arr[j][k] and arr[j][k] != arr[r-1][c-1]:
+                if arr[0][0] != arr[i][j] and arr[i][j] != arr[k][l] and arr[k][l] != arr[r-1][c-1]:
                     result += 1
 print(result)
